3f8b7aa7fa47b68fbcfed68edf742262d6055658
Matt Langshur <matthewlangshur@gmail.com>
2020-10-06 19:31:31 -0400

    Re-enabled the data base for the application

--- deployHeroku.sh
+++ deployHeroku.sh
@@ -1,3 +1,6 @@
-cd Artsee-Backend
-./gradlew clean
-./gradlew build
+# to run this just do bash ./deployHeroku.sh
+# Make sure that your changes are pushed to main tho
+heroku config:add PROCFILE=Artsee-Backend/Procfile --app artsee-backend-2020
+heroku git:remote --app artsee-backend-2020 --remote backend-heroku
+git remote -v
+git push backend-heroku master
