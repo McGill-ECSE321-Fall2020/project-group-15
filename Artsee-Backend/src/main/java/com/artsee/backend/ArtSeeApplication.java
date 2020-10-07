a1e138a011b2011adfb4f2d8471436deee8b0283
Matt Langshur <matthewlangshur@gmail.com>
2020-10-07 18:49:50 -0400

    Updated main controller

--- Artsee-Backend/src/main/java/com/artsee/backend/ArtSeeApplication.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/ArtSeeApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class ArtSeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtSeeApplication.class, args);
 	}
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String homepage() {
 		return "Hello! This is the Artsee application!";
 	}
