d97077c0baf54274384262113aa4b51798d88779
danielschwartz3 <71156919+danielschwartz3@users.noreply.github.com>
2020-10-08 23:08:52 -0400

    Fixing Apllication bug

--- Artsee-Backend/src/main/java/com/artsee/backend/model/Application.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/model/Application.java
@@ -1,26 +1,27 @@
-//package com.artsee.backend.model;
-//
-//import javax.persistence.Entity;
-//
-//import java.util.Set;
-//import javax.persistence.CascadeType;
-//import javax.persistence.OneToMany;
-//import javax.persistence.Id;
-//
-//
-//@Entity
-//public class Application{
+package com.artsee.backend.model;
+
+import javax.persistence.Entity;
+
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+import javax.persistence.Id;
+
+
+@Entity
+public class Application{
+
+
+@Id private String name; 
+public String getName() {
+	return this.name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
 //private Set<User> users;
-//
-//
-//@Id private String name; 
-//public String getName() {
-//	return this.name;
-//}
-//public void setName(String name) {
-//	this.name = name;
-//}
-//
+
 //@OneToMany(cascade={CascadeType.ALL})
 //public Set<User> getUsers() {
 //   return this.users;
@@ -29,7 +30,7 @@
 //public void setUsers(Set<User> userss) {
 //   this.users = userss;
 //}
-//
+
 //private Set<Order> orders;
 //
 //@OneToMany(cascade={CascadeType.ALL})
@@ -40,7 +41,7 @@
 //public void setOrders(Set<Order> orderss) {
 //   this.orders = orderss;
 //}
-//
+
 //private Set<Artwork> artworks;
 //
 //@OneToMany(cascade={CascadeType.ALL})
@@ -51,5 +52,5 @@
 //public void setArtworks(Set<Artwork> artworkss) {
 //   this.artworks = artworkss;
 //}
-//
-//}
+
+}
