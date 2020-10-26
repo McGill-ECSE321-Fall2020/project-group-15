8b1e7878b76d73ddf6721c3ac600e3962608da5d
ashaduzzamane <enan@live.ca>
2020-10-26 03:48:04 -0400

    create foundation/boilerplate for service layer and controller

--- Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
@@ -1,4 +1,16 @@
 package com.artsee.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.artsee.backend.service.ArtseeService;
+
+@CrossOrigin(origins = "*")
+@RestController
 public class ArtseeRestController {
+
+	@Autowired
+	private ArtseeService service;
+
 }
