be826a3b8b40bc7c145e2492ae6c3c5680798ec2
ashaduzzamane <enan@live.ca>
2020-10-26 17:52:53 -0400

    small changes

--- Artsee-Backend/src/main/java/com/artsee/backend/dao/ReviewRepository.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/dao/ReviewRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.artsee.backend.model.Review;
 
 
-@RepositoryRestResource(collectionResourceRel = "review_data", path = "review_data")
+//@RepositoryRestResource(collectionResourceRel = "review_data", path = "review_data")
 public interface ReviewRepository extends CrudRepository<Review, Integer>{
 	//Perform CRUD operations by interacting with the Review class
 	Review findReviewByReviewID(Integer reviewID);
