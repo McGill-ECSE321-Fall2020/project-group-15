28af6e79df90140653470b6b4951af93ecd442e3
ashaduzzamane <enan@live.ca>
2020-10-26 03:52:42 -0400

    edit controller boiler plate and edit gradle file to include rest

--- Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
@@ -1,8 +1,22 @@
 package com.artsee.backend.controller;
 
+import java.sql.Date;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.artsee.backend.model.*;
+import com.artsee.backend.dto.*;
 
 import com.artsee.backend.service.ArtseeService;
 
