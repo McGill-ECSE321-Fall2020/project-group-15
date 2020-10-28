a299e97498bb00fcd37420c353820fd362faa801
ashaduzzamane <enan@live.ca>
2020-10-28 00:34:58 -0400

    create Artist DTO

--- Artsee-Backend/src/main/java/com/artsee/backend/dto/ArtistDto.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/dto/ArtistDto.java
@@ -1,8 +1,55 @@
 package com.artsee.backend.dto;
 
-import java.sql.Date;
-
 public class ArtistDto {
 
+	private String artistID;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private AddressDto address;
+	
+	public ArtistDto() {
+		
+	}
+	
+	public ArtistDto(String artistID, String email, String password, String firstName, String lastName, String phoneNumber, AddressDto address) {
+		this.artistID = artistID;
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+	
+	public String getArtistID() {
+		return artistID;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public AddressDto getAddress() {
+		return address;
+	}
 	
 }
