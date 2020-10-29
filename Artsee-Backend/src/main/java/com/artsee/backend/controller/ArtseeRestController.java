ed5fbbf0f4f0402b7162b62d2ba64f9e7baf1986
Ragheed Qasmieh <ragheed.qasmieh@mail.mcgill.ca>
2020-10-29 17:44:02 -0400

    Working on api.

--- Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
+++ Artsee-Backend/src/main/java/com/artsee/backend/controller/ArtseeRestController.java
@@ -7,11 +7,13 @@ import com.artsee.backend.dto.ArtistDto;
 import com.artsee.backend.dto.CustomerDto;
 import com.artsee.backend.dto.EndUserDto;
 import com.artsee.backend.dto.SignInDto;
+import com.artsee.backend.dto.ArtworkDto;
 import com.artsee.backend.model.Address;
 import com.artsee.backend.model.Administrator;
 import com.artsee.backend.model.Artist;
 import com.artsee.backend.model.Customer;
 import com.artsee.backend.model.EndUser;
+import com.artsee.backend.model.Artwork;
 import com.artsee.backend.service.ArtseeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -27,8 +29,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.stream.Collectors;
+import java.util.List;
+import java.util.ArrayList;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -66,15 +71,27 @@ public class ArtseeRestController {
 		return new AddressDto(address.getAddressID(),address.getAddressLine1(), address.getAddressLine2(), address.getCity(), address.getProvince(), address.getPostalCode(), address.getCountry());
 	}
 	
-//	@GetMapping(value = { "/artworks", "/artworks/" })
-//    public List<ArtworkDto> getAllArtworks() {
-//        return service.getAllArtworks().stream().map(a -> convertToDto(a)).collect(Collectors.toList());
-//    }
-//
-//    @GetMapping(value = { "/artworks/{id}", "/artworks/{id}/" })
-//    public ArtworkDto getArtworkById(@PathVariable("id") int id) throws IllegalArgumentException {
-//        return convertToDto(service.getArtworkById(id));
-//    }
+	public ArtworkDto convertToDto(Artwork artwork, Artist artist) {
+		ArtistDto aDto = convertToDto(artist);
+		return new ArtworkDto(artwork.getArtworkID(), artwork.getName(), artwork.getDescription(), artwork.getPrice(), artwork.getDateOfCreation(), 
+				artwork.getNumInStock(), aDto);
+	}
+	
+	@GetMapping(value = { "/artworks", "/artworks/" })
+    public List<ArtworkDto> getAllArtworks() {
+		List<Artwork> artworks = service.getAllArtworks();
+		List<ArtworkDto> aDtos = new ArrayList<>();
+		for(Artwork a: artworks) {
+			aDtos.add(convertToDto(a, a.getArtist()));
+		}
+        return aDtos;
+    }
+
+    @GetMapping(value = { "/artworks/{id}", "/artworks/{id}/" })
+    public ArtworkDto getArtworkById(@PathVariable("id") int id) throws IllegalArgumentException {
+    	Artwork a = service.getArtworkById(id);
+        return convertToDto(a, a.getArtist());
+    }
 
 //    @GetMapping(value = { "/artworks/{artist_id}", "/artworks/{artist_id}/" })
 //    public ArtworkDto getArtworkById(@PathVariable("id") String id) throws IllegalArgumentException {
