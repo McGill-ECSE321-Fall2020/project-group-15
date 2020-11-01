9fcb224fc928c78895e6b92698aec9ff88b4da29
Matt Langshur <matthewlangshur@gmail.com>
2020-11-01 13:13:39 -0500

    completed first integration test

--- Artsee-Backend/src/test/java/com/artsee/backend/service/TestArtseeServiceMethods.java
+++ Artsee-Backend/src/test/java/com/artsee/backend/service/TestArtseeServiceMethods.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Optional;
 
-import org.apache.tomcat.jni.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
