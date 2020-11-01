641a70d5d12e5a8ca94501456545facc96ac2b1f
Matt Langshur <matthewlangshur@gmail.com>
2020-10-31 20:46:50 -0400

    set up artwork order tests

--- Artsee-Backend/src/test/java/com/artsee/backend/service/TestArtworkOrderService.java
+++ Artsee-Backend/src/test/java/com/artsee/backend/service/TestArtworkOrderService.java
@@ -19,6 +19,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Optional;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import org.apache.tomcat.jni.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -37,6 +40,56 @@ import com.artsee.backend.dao.*;
 import com.artsee.backend.utility.*;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
+@ExtendWith(MockitoExtension.class)
 public class TestArtworkOrderService {
+	
+	@Mock
+	private ArtworkOrderRepository artworkOrderDao;
+	
+	@InjectMocks
+	private ArtseeService service;
+	
+	public static final int ID = 1234;
+	public static final DeliveryMethod DELIVERYMETHOD = DeliveryMethod.SHIP;
+	public static final Customer CUSTOMER = new Customer();
+	public static final List<Artwork> ARTLIST = new ArrayList<Artwork>();
 
+	public static final String CUSTOMER_ID = "123";
+	
+	@BeforeEach
+	public void setMockOuput() {
+
+		CUSTOMER.setUserID(CUSTOMER_ID);
+		ARTLIST.add(new Artwork());
+		ARTLIST.add(new Artwork());
+
+		ARTLIST.get(0).setArtworkID(1);
+		ARTLIST.get(1).setArtworkID(2);
+
+		
+		lenient().when(artworkOrderDao.findByCustomer(CUSTOMER)).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(CUSTOMER)) {
+                List<ArtworkOrder> artOrderList = new ArrayList<ArtworkOrder>();
+                artOrderList.add(TestUtility.createArtworkOrder(ID, DELIVERYMETHOD, CUSTOMER, ARTLIST));
+                return artOrderList;
+            } else {
+                return null;
+            }
+		});
+		
+		lenient().when(artworkOrderDao.findById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+            if (invocation.getArgument(0).equals(ID)) {
+                return Optional.of(
+					TestUtility.createArtworkOrder(ID, DELIVERYMETHOD, CUSTOMER, ARTLIST));
+            } else {
+                return Optional.empty();
+            }
+        });
+		
+		lenient().when(artworkOrderDao.save(any(ArtworkOrder.class))).thenAnswer((InvocationOnMock invocation) -> {
+			return invocation.getArgument(0);
+		});
+
+    }
+    
 }
