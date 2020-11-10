8e2092ab09e46cdd6b02fc824cc4f9a41648b28d
ashaduzzamane <enan@live.ca>
2020-11-10 03:50:03 -0500

    refactor login page

--- Artsee-Frontend/src/components/Signup.vue
+++ Artsee-Frontend/src/components/Signup.vue
@@ -1,64 +1,79 @@
 <template>
-    <div class="signup-container">
-        <div class="card w-25">
-            <div class="card-body">
-                <img class="imageStyle" src="@/assets/logo.png">
-                <form>
-                    <div class="row">
-                        <div class="col">
-                            <input type="text" class="form-control" placeholder="First name">
+    <div>
+        <head>
+        <title>Sign Up Page</title>
+        </head>
+        <body>
+            <div class="card-container">
+                <div class="card w-25">
+                    <div class="card-body">
+                        <div class="image-container">
+                        <img class="image-style" src="@/assets/logo.png">
                         </div>
-                        <div class="col">
-                            <input type="text" class="form-control" placeholder="Last name">
-                        </div>
+                        <form>
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="First name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Last name">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address">
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Re-enter Password">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Address Line 1">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Address Line 2">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="City">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Province">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Postal Code">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Country">
+                            </div>
+                            <div class="button-container">
+                                <div>
+                                    <button type="submit" class="btn btn-light signin-btn-style">Sign In Instead</button>
+                                </div>
+                                <div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Re-enter Password">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Address Line 1">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Address Line 2">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="City">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Province">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Postal Code">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Country">
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <button type="submit" class="btn btn-light signinBtn">Sign In Instead</button>
-                        </div>
-                        <div class="col">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
+        </body>
     </div>
 </template>
 
 <script>
-
+    
 </script>
 
 <style>
-    .signup-container {
+    body {
+        background-image: linear-gradient(to right, #BC54C2, #5056CD);
+    }
+    .card-container {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -66,13 +81,23 @@
     .row {
         margin-bottom: 10px;
     }
-    .imageStyle {
+    .image-style {
         width: 60%;
+        margin: 10px 0 25px 0
     }
-    .signinBtn {
+    .image-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    .signin-btn-style {
         color: #037BFF;
     }
-    .signinBtn:hover {
+    .signin-btn-style:hover {
         color: #037BFF;
+    }
+    .button-container {
+        display: flex;
+        justify-content: space-around;
     }
 </style>
