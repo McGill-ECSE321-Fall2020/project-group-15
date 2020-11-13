c2fa0f1f1000cd19dad21e74a17c26551365fe94
ashaduzzamane <enan@live.ca>
2020-11-13 03:21:11 -0500

    add vuex for state management

--- Artsee-Frontend/src/main.js
+++ Artsee-Frontend/src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import BootstrapVue from "bootstrap-vue"
 import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+Vue.use(Vuex)
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
